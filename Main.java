c1b19d0975349fe6682ba29e9fce8777d6bfffb1
stud <jennifer.korbmacher@edu.bib.de>
2020-09-07 09:03:29 +0200

    Sei ein Macher-Ausgabe verändert

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package seimacher;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Sei ein Macher!");
+		System.out.println("Sei ein Macher! Wie ein Seimacher!");
 	}
 
 }
